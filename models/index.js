a2e71b1d17f297fd68a38f58e47bc42627cfa885
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-10-31 18:04:05 -0400

    set up models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// Product.belongsTo(Category, {
+//   foreignKey: 'category_id',
+// });
 
 // Categories have many Products
-
+// Category.hasMany(Product, {
+//   foreignKey: 'category_id',
+//   onDelete: 'CASCADE',
+// });
 // Products belongToMany Tags (through ProductTag)
-
+// Product.belongToMany(Tag, {
+//   foreignKey: 'category_id',
+// })
 // Tags belongToMany Products (through ProductTag)
-
+// Tag.belongToMany(Product, {
+//   foreignKey
+// })
 module.exports = {
   Product,
   Category,
